84b972f41e56753509cd87e3a15ea51c0aefd460
mucnjakf <filip.mucnjak@outlook.com>
2025-02-27 16:44:21 +0100

    Implemented update customer endpoint

---
+++ billtracker-api/Endpoints/Customers/UpdateCustomerEndpoint.cs
@@ -0,0 +1,53 @@
+using billtracker_api.Database;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace billtracker_api.Endpoints.Customers;
+
+internal sealed record UpdateCustomerRequest
+{
+	[FromRoute]
+	public int CustomerId { get; set; }
+
+	public string Name { get; set; } = null!;
+
+	public string Surname { get; set; } = null!;
+
+	public string Email { get; set; } = null!;
+
+	public string Telephone { get; set; } = null!;
+
+	public int? CityId { get; set; }
+}
+
+internal sealed class UpdateCustomerEndpoint(AppDbContext appDbContext) : Endpoint<UpdateCustomerRequest, Results<NoContent, NotFound>>
+{
+	public override void Configure()
+	{
+		Put("/api/customers/{customerId}");
+		AllowAnonymous();
+	}
+
+	public override async Task<Results<NoContent, NotFound>> ExecuteAsync(UpdateCustomerRequest req, CancellationToken ct)
+	{
+		var customer = await appDbContext.Customers.FindAsync([req.CustomerId], cancellationToken: ct);
+
+		if (customer is null)
+		{
+			return TypedResults.NotFound();
+		}
+
+		var city = await appDbContext.Cities.FindAsync([req.CityId], cancellationToken: ct);
+
+		customer.Name = req.Name;
+		customer.Surname = req.Surname;
+		customer.Email = req.Email;
+		customer.Telephone = req.Telephone;
+		customer.CityId = city?.Id;
+
+		await appDbContext.SaveChangesAsync(ct);
+
+		return TypedResults.NoContent();
+	}
+}
