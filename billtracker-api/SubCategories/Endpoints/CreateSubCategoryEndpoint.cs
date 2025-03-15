68d3e33dcab7edc89a82047ab33d55fb7c3a84a1
mucnjakf <filip.mucnjak@outlook.com>
2025-03-15 11:55:37 +0100

    Implemented create sub-category endpoint

---
+++ billtracker-api/SubCategories/Endpoints/CreateSubCategoryEndpoint.cs
@@ -0,0 +1,52 @@
+using billtracker_api.Auth;
+using billtracker_api.Database;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace billtracker_api.SubCategories.Endpoints;
+
+internal sealed record CreateSubCategoryRequest(
+	string Name,
+	int CategoryId);
+
+internal sealed class CreateSubCategoryEndpoint(AppDbContext appDbContext)
+	: Endpoint<CreateSubCategoryRequest, Results<Created<SubCategoryDto>, NotFound>>
+{
+	public override void Configure()
+	{
+		Roles(AppRoles.User);
+		Post(AppRoutes.SubCategories);
+		Description(x => x.WithTags(AppRouteTags.SubCategories));
+	}
+
+	public override async Task<Results<Created<SubCategoryDto>, NotFound>> ExecuteAsync(CreateSubCategoryRequest req, CancellationToken ct)
+	{
+		var category = await appDbContext.Categories
+			.AsNoTracking()
+			.SingleOrDefaultAsync(x => x.Id == req.CategoryId, ct);
+
+		if (category is null)
+		{
+			return TypedResults.NotFound();
+		}
+
+		var subCategory = new SubCategory
+		{
+			Name = req.Name,
+			CategoryId = category.Id
+		};
+
+		await appDbContext.SubCategories.AddAsync(subCategory, ct);
+		await appDbContext.SaveChangesAsync(ct);
+
+		subCategory = await appDbContext.SubCategories
+			.AsNoTracking()
+			.Include(x => x.Category)
+			.SingleOrDefaultAsync(x => x.Id == subCategory.Id, ct);
+
+		var subCategoryDto = subCategory!.ToSubCategoryDto();
+
+		return TypedResults.Created($"{HttpContext.Request.Host}/api/subcategories/{subCategoryDto.Id}", subCategoryDto);
+	}
+}
