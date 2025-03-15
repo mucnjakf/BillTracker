cc54a6764f8552cb72043230a803d53d127876b5
mucnjakf <filip.mucnjak@outlook.com>
2025-03-15 11:51:52 +0100

    Implemented get sub-category endpoint

---
+++ billtracker-api/SubCategories/Endpoints/GetSubCategoryEndpoint.cs
@@ -0,0 +1,43 @@
+using billtracker_api.Auth;
+using billtracker_api.Database;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace billtracker_api.SubCategories.Endpoints;
+
+internal sealed record GetSubCategoryRequest
+{
+	[RouteParam]
+	public int SubCategoryId { get; init; }
+}
+
+internal sealed class GetSubCategoryEndpoint(AppDbContext appDbContext)
+	: Endpoint<GetSubCategoryRequest, Results<Ok<SubCategoryDto>, NotFound>>
+{
+	public override void Configure()
+	{
+		Roles(AppRoles.User);
+		Get($"{AppRoutes.SubCategories}/{{subCategoryId}}");
+		Description(x => x.WithTags(AppRouteTags.SubCategories));
+	}
+
+	public override async Task<Results<Ok<SubCategoryDto>, NotFound>> ExecuteAsync(
+		GetSubCategoryRequest req,
+		CancellationToken ct)
+	{
+		var subCategory = await appDbContext.SubCategories
+			.AsNoTracking()
+			.Include(x => x.Category)
+			.SingleOrDefaultAsync(x => x.Id == req.SubCategoryId, ct);
+
+		if (subCategory is null)
+		{
+			return TypedResults.NotFound();
+		}
+
+		var subCategoryDto = subCategory.ToSubCategoryDto();
+
+		return TypedResults.Ok(subCategoryDto);
+	}
+}
