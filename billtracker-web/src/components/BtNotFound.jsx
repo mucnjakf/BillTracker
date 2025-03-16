0ca822126fa57aac2886b820d425d89f14297d57
mucnjakf <filip.mucnjak@outlook.com>
2025-03-16 20:51:44 +0100

    Implemented not found page

--- billtracker-web/src/components/BtNotFound.jsx
+++ billtracker-web/src/components/BtNotFound.jsx
@@ -1,5 +1,23 @@
+import { BsExclamationCircle } from 'react-icons/bs'
+
 const NotFound = () => {
-  return <h1>Not found</h1>
+  return (
+    <>
+      <h1 className="mb-3">
+        <BsExclamationCircle/>
+        <span className="ms-3">Page not found</span>
+      </h1>
+
+      <p className="text-muted small">HTTP status: 404</p>
+
+      <p>
+        The page you are looking for may have been moved, renamed, or no longer exists.
+      </p>
+      <p>
+        Please check the URL and try again, or return to <a href="/" className="text-decoration-none fw-bold">home</a>.
+      </p>
+    </>
+  )
 }
 
 export default NotFound
